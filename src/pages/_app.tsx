db489cb72fdb28d4bf17aa637713a76c7989174c
Karim <karimhammaidi3@gmail.com>
2026-09-04 16:02:35 +0200

    Remove the "Poser une question" floating support widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app';
 import { LanguageProvider } from '../client/i18n/LanguageContext';
-import AssistanceFlottante from '../client/components/Assistance/AssistanceFlottante';
 import '../global.css';
-import '../client/components/Assistance/AssistanceFlottante.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
       <Component {...pageProps} />
-      <AssistanceFlottante />
     </LanguageProvider>
   );
 }
